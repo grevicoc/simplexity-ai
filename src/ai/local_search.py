bbdd29a89130b3cdf92e93620d7cecd4441a5d05
Muhammad Tito Prakasa <muhammad.prakasa@gdn-commerce.com>
2021-10-05 13:15:19 +0700

    1/4 progress

--- src/ai/local_search.py
+++ src/ai/local_search.py
@@ -1,8 +1,9 @@
 import random
 from time import time
 
-from src.constant import ShapeConstant
+from src.constant import GameConstant, ShapeConstant
 from src.model import State
+from utility import is_out
 
 from typing import Tuple, List
 
@@ -17,3 +18,26 @@ class LocalSearch:
         best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
 
         return None
+    
+    #untuk menentukan shapenya blank atau tidak
+    def is_blank(self, state: State, col: int, row: int):
+        return state.board.board[col][row].shape==ShapeConstant.BLANK
+
+    #untuk 
+    def calculateValue(self, state: State, col: int, row: int, resultList: List[Tuple[int, int, bool]]):
+        resultList.append((col, 0, False) if is_out(state.board,row,col) and self.is_blank(state,col,row) else None)        #blm selesai
+
+    #untuk mengisi value tiap-tiap column
+    def fillValueCells(self, state: State):
+        #tuple 3 elemen, (column: int, value: int, is_attack: bool)
+        listOfValue = []
+        for i in range (state.board.col):
+            if (state.board.board[i][6].shape!=ShapeConstant.BLANK):
+                self.calculateValue(state=state,col=i,row=6,resultList=listOfValue)     #blm selesai
+                return None
+            else:
+                listOfValue.append((i,0,False)) #nilai terendah, taro di paling belakang gapapa
+
+    
+
+    
